891a392d1420f6ddc2bc3d2e69a7a01bf8e73bf9
Your Name <you@example.com>
2024-11-16 23:29:12 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+// import React from 'react'
+import type { Metadata } from "next";
+import {Plus_Jakarta_Sans} from "next/font/google"
+import "./globals.css";
+import {cn} from '@/lib/utils'
+import {ThemeProvider} from '@/components/theme-provider'
+
+const fontSans = Plus_Jakarta_Sans ({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: '--font-sans'
+
+});
+
+export const metadata: Metadata = {
+  title: "Carepulse",
+  description: "A healthcare management system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="eng">
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.
+        variable)}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark">
+            {children}
+          </ThemeProvider>
+        </body>
+    </html>
+  );
+}
+
+
+
+
+
+
